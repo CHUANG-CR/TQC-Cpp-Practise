21af7df85c62419ffe05b0e0dfe4a0e97f2f44e9
Ren <4B3G0118@office.stust.edu.tw>
2026-06-05 22:15:02 +0800

    Create 分數調整.cpp

---
+++ 分數調整.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int score;
+    if (cin >> score) {
+        // 判斷是否在 0~100 的合理範圍內
+        if (score < 0 || score > 100) {
+            cout << "error" << endl;
+        } else {
+            // 依據分數區間進行加分
+            if (score > 60) {
+                score += 10;
+            } else {
+                score += 5;
+            }
+            cout << score << endl;
+        }
+    }
+    return 0;
+}
