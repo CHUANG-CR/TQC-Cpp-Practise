ed56e71c9d9201fe35b768a8fc9f4f3c36e2731a
Ren <4B3G0118@office.stust.edu.tw>
2026-06-05 22:24:46 +0800

    Create 二轉成十進位.cpp

---
+++ 二轉成十進位.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string binStr;
+    if (cin >> binStr) {
+        int decValue = 0;
+        
+        // 利用迴圈手動將二進位轉十進位
+        for (int i = 0; i < binStr.length(); i++) {
+            // 每次向左移一位 (乘2)，再加上當前位數的值 (0或1)
+            decValue = decValue * 2 + (binStr[i] - '0');
+        }
+        
+        cout << decValue << endl;
+        
+        /* // 也可以使用 C++ 內建函式 stoi 直接轉換 (更為簡潔)：
+        // int decValue = stoi(binStr, nullptr, 2);
+        // cout << decValue << endl;
+        */
+    }
+    return 0;
+}
